dd9c9f7b577e5094738ca525d45f2a6fcc1d70e8
caodonglei-kavout <caodonglei@gmail.com>
2022-10-25 23:12:37 +0800

    Create Reward.sol
    智能合约：评奖，如果试卷得分超过及格线，则mint NFT

---
+++ contracts/Reward.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.8.13;
+import "./CheckAnswer.sol";
+
+contract Reward {
+    address owner;
+    mapping(uint8 => mapping(uint8 => uint8)) passLines;
+
+    event Reward(address indexed sender, uint8 type, uint8 level, uint8 score, bool pass);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setPassLine(uint8 _type, utin8 _level, uint8 _score) public {
+        require(owner == msg.sender);
+        passLines[_type][_level] = _score;
+    }
+
+    function getReward(uint8 _type, uint8 _level, address[] _questions, uint8[] _answers) public {
+        uint8 score = getScore(_answers);
+        uint8 passLine = passLines[_type][_level]
+        if (score >= passLine) {
+            mint(user, _type, _level, score);
+
+        }
+        
+        emit Reward(msg.sender, _type, _level, score, score >= passLine);
+    }
+}
