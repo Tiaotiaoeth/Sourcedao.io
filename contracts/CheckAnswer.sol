71ee4e22552c1013a7c8ad862a7aa5d3d2da2da8
caodonglei-kavout <caodonglei@gmail.com>
2022-10-26 00:32:31 +0800

    MOD: 重构题库

--- contracts/CheckAnswer.sol
+++ contracts/CheckAnswer.sol
@@ -1,23 +1,23 @@
 pragma solidity ^0.8.13;
+import "./QuestionRepo.sol";
 
 contract CheckAnswer {
     address owner;
-    address answerRepo;
 
     constructor() {
         owner = msg.sender;
     }
 
-    function setRepoAddr(address _repoAddr) public {
-        require(owner == msg.sender);
-        answerRepo = _repoAddr;
-    }
-
-    function getScore(address[] questions, uint8[] _answers) external view returns (uint8) {
+    function getScore(
+        uint8 _type, 
+        uint8 _level, 
+        uint256[] _indices, 
+        uint8[] _answers
+    ) external view returns (uint8) {
         uint8 score = 0;
         for (uint i = 0; i < _answers.length; i++) {
-            // TODO: get right answer
-            if (rightAnswer == _answers[i]) {
+            uint8 standard = getStandardAnswer(_type, _level, _indices[i]);
+            if (standard == _answers[i]) {
                 score += 1;
             }
         }
