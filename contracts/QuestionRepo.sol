dea4af6d542e87a1bc0038bf9cf94541af68839a
caodonglei-kavout <caodonglei@gmail.com>
2022-10-26 00:30:37 +0800

    MOD：add event of selected question

--- contracts/QuestionRepo.sol
+++ contracts/QuestionRepo.sol
@@ -10,6 +10,7 @@ contract QuestionRepo {
     mapping(uint8 => mapping(uint8 => Question[])) questions;       
 
     event AddQuestion(address indexed sender, uint8 type, uint8 level);
+    event SelectQuestion(address indexed sender);
 
     constructor() {
         owner = msg.sender;
@@ -29,17 +30,14 @@ contract QuestionRepo {
         emit AddQuestion(_author, _type, _level);
     }
 
-    // 生成一次测试
-    function getQuestion(uint8 _type, uint8 _level, uint _index) view returns (Question) {
-        return ;
-    }
-
     // TODO：边界检查
     function randQuestionHash(uint8 _type, uint8 _level) public returns (uint256, string) {
         uint256 length = questions[_type][_level].length;
         uint256 random = uint256(keccak256(abi.encodePacked(block.difficulty, now)));
         uint256 index = random % length;
         return (index, questions[_type][_level][index]._ipfshash);
+
+        emit SelectQuestion(questions[_type][_level][index]._author);
     }
 
     function getStandardAnswer(uint8 _type, uint8 _level, uint256 _index) public returns (uint8) {
